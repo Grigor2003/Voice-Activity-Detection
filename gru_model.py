c6cca0b6483f8546082b53e92c387da6eb8aed3c
Aleph-Zero0 <narek.ginoyan.1000@gmail.com>
2024-07-12 23:04:58 +0400

    added new model

--- gru_model.py
+++ gru_model.py
@@ -2,13 +2,33 @@ import torch.nn as nn
 
 
 class SimpleG(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers=1):
+    def __init__(self, input_dim, hidden_dim, gru_num_layers=1):
         super(SimpleG, self).__init__()
-        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.gru = nn.GRU(input_dim, hidden_dim, gru_num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_dim, 1)
         self.input_dim = input_dim
 
     def forward(self, x):
         out, _ = self.gru(x)
         out = self.fc(out)
+        return out
+
+
+class SimpleDGGD(nn.Module):
+    def __init__(self, input_dim, hidden_dim1, hidden_dim2, hidden_dim3, hidden_dim4, gru1_num_layers=1, gru2_num_layers=1):
+        super(SimpleDGGD, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim1)
+        self.gru1 = nn.GRU(hidden_dim1, hidden_dim2, gru1_num_layers, batch_first=True)
+        self.gru2 = nn.GRU(hidden_dim2, hidden_dim3, gru2_num_layers, batch_first=True)
+        self.fc2 = nn.Linear(hidden_dim3, hidden_dim4)
+        self.fc3 = nn.Linear(hidden_dim4, 1)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = F.relu(out)
+        out, _ = self.gru1(out)
+        out, _ = self.gru2(out)
+        out = self.fc2(out)
+        out = F.relu(out)
+        out = self.fc3(out)
         return out
--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-from gru_model import SimpleG
+from gru_model import SimpleG, SimpleDGGD
 
 simple_gru = SimpleG(input_dim=64, hidden_dim=48)
+simple_gru_with_denses = SimpleDGGD(input_dim=64, hidden_dim1=32, hidden_dim2=32, hidden_dim3=16, hidden_dim4=8)
 
 MODELS = {
     "SimpleG_64_48": simple_gru,
+    "SimpleDGGD_64_32_32_16_8": simple_gru_with_denses,
     "Other": None
 }
 
