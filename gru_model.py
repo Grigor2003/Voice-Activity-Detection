2a77559cf18acaaebe42a930b6206f2cf497a1f0
Aleph-Zero0 <narek.ginoyan.1000@gmail.com>
2024-07-12 23:19:30 +0400

    added another model

--- gru_model.py
+++ gru_model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class SimpleG(nn.Module):
@@ -6,6 +7,7 @@ class SimpleG(nn.Module):
         super(SimpleG, self).__init__()
         self.gru = nn.GRU(input_dim, hidden_dim, gru_num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_dim, 1)
+
         self.input_dim = input_dim
 
     def forward(self, x):
@@ -15,13 +17,16 @@ class SimpleG(nn.Module):
 
 
 class SimpleDGGD(nn.Module):
-    def __init__(self, input_dim, hidden_dim1, hidden_dim2, hidden_dim3, hidden_dim4, gru1_num_layers=1, gru2_num_layers=1):
+    def __init__(self, input_dim, hidden_dim1, hidden_dim2, hidden_dim3, hidden_dim4, gru1_num_layers=1,
+                 gru2_num_layers=1):
         super(SimpleDGGD, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim1)
         self.gru1 = nn.GRU(hidden_dim1, hidden_dim2, gru1_num_layers, batch_first=True)
         self.gru2 = nn.GRU(hidden_dim2, hidden_dim3, gru2_num_layers, batch_first=True)
         self.fc2 = nn.Linear(hidden_dim3, hidden_dim4)
         self.fc3 = nn.Linear(hidden_dim4, 1)
+
+        self.input_dim = input_dim
 
     def forward(self, x):
         out = self.fc1(x)
@@ -30,5 +35,35 @@ class SimpleDGGD(nn.Module):
         out, _ = self.gru2(out)
         out = self.fc2(out)
         out = F.relu(out)
+        out = self.fc3(out)
+        return out
+
+
+class DGGD(nn.Module):
+    def __init__(self, input_dim, hidden_dim1, hidden_dim2, hidden_dim3, hidden_dim4, num_layers=1, dropout_prob=0.5):
+        super(DGGD, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim1)
+        self.layernorm1 = nn.LayerNorm(hidden_dim1)
+        self.dropout1 = nn.Dropout(dropout_prob)
+        self.gru1 = nn.GRU(hidden_dim1, hidden_dim2, num_layers, batch_first=True)
+        self.gru2 = nn.GRU(hidden_dim2, hidden_dim3, num_layers, batch_first=True)
+        self.fc2 = nn.Linear(hidden_dim3, hidden_dim4)
+        self.layernorm2 = nn.LayerNorm(hidden_dim4)
+        self.dropout2 = nn.Dropout(dropout_prob)
+        self.fc3 = nn.Linear(hidden_dim4, 1)
+
+        self.input_dim = input_dim
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = F.relu(out)
+        out = self.layernorm1(out)
+        out = self.dropout1(out)
+        out, _ = self.gru1(out)
+        out, _ = self.gru2(out)
+        out = self.fc2(out)
+        out = F.relu(out)
+        out = self.layernorm2(out)
+        out = self.dropout2(out)
         out = self.fc3(out)
         return out
--- models.py
+++ models.py
@@ -1,11 +1,13 @@
-from gru_model import SimpleG, SimpleDGGD
+from gru_model import SimpleG, SimpleDGGD, DGGD
 
 simple_gru = SimpleG(input_dim=64, hidden_dim=48)
 simple_gru_with_denses = SimpleDGGD(input_dim=64, hidden_dim1=32, hidden_dim2=32, hidden_dim3=16, hidden_dim4=8)
-
+gru_with_denses = DGGD(input_dim=64, hidden_dim1=32, hidden_dim2=32, hidden_dim3=16,
+                       hidden_dim4=8)
 MODELS = {
     "SimpleG_64_48": simple_gru,
     "SimpleDGGD_64_32_32_16_8": simple_gru_with_denses,
+    "DGGD_64_32_32_16_8": gru_with_denses,
     "Other": None
 }
 
