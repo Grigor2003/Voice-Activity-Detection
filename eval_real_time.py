39951e172d6ac83d66adc805116e7e4abfac9e89
Micra <72358531+Grigor2003@users.noreply.github.com>
2024-11-03 15:12:02 +0400

    real time evaluation

---
+++ eval_real_time.py
@@ -0,0 +1,82 @@
+import os
+import queue
+import time
+
+import numpy as np
+import sounddevice as sd
+import torch
+
+from models_handler import MODELS
+from other.utils import find_last_model_in_tree, WaveToMFCCConverter
+
+model_name = r"WhisperLike_64"
+train_res_dir = "train_results"
+th = 0.55
+
+model_trains_tree_dir = os.path.join(train_res_dir, model_name)
+
+model_new_dir, model_path = find_last_model_in_tree(model_trains_tree_dir)
+
+if model_path is None:
+    raise Exception(f"No model was found at {model_trains_tree_dir}")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Device: {device}")
+
+checkpoint = torch.load(model_path, weights_only=True)
+
+sample_rate = checkpoint['mfcc_sample_rate']
+win_lenght = checkpoint['mfcc_win_length']
+hop_lenght = checkpoint['mfcc_hop_length']
+
+print("sample_rate:", sample_rate)
+if 2 * hop_lenght != win_lenght:
+    raise Exception("works only when the hop_lenght is half of the win_lenght")
+
+model = MODELS[model_name]().to(device)
+model.load_state_dict(checkpoint['model_state_dict'])
+
+mfcc_converter = WaveToMFCCConverter(
+    n_mfcc=checkpoint['mfcc_n_mfcc'],
+    sample_rate=sample_rate,
+    win_length=win_lenght,
+    hop_length=hop_lenght)
+
+model.eval()
+
+q = queue.Queue()
+
+
+def callback(overlap_frame, frame_len, time_info, status):
+    """This is called (from a separate thread) for each audio block."""
+    q.put(overlap_frame.copy())
+
+
+frames = []
+last_frame = None
+
+with sd.InputStream(samplerate=sample_rate, blocksize=hop_lenght, channels=1, callback=callback):
+    while True:
+        try:
+            overlap_frame = q.get(block=False)
+
+            if last_frame is not None:
+                frame = np.concatenate([last_frame, overlap_frame], axis=0)
+                wave = torch.from_numpy(frame).squeeze(-1)
+                spectrogram = mfcc_converter(wave).to(device)
+
+                frames.append(spectrogram)
+                inp_tensor = torch.cat(frames[-50:]).unsqueeze(0)
+                st = time.time()
+                out = model(inp_tensor).detach().cpu().numpy().flatten()[-2:]
+                en = time.time()
+                print(f"{out}, in {(en - st) * 1000:.2f} ms")
+
+            last_frame = overlap_frame.copy()
+
+            size = q.qsize()
+            if size > 1:
+                print(f"Queued {size} frames")
+
+        except queue.Empty:
+            pass
